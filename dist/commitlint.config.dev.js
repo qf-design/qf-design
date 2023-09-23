1ffaeff6684c0979805595ee5018ea1509208d40
wangkai <1205669217@qq.com>
2023-09-23 18:04:28 +0800

    feat: 1

--- dist/commitlint.config.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-// eslint-disable-next-line no-undef
-module.exports = {
-  "extends": ["@commitlint/config-conventional"],
-  rules: {
-    "subject-case": [2, "never", ["upper-case", "pascal-case", "start-case"]]
-  }
-};
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,5 @@
 import DefaultTheme from 'vitepress/theme';
-import qfDesignTest from 'qf-design-test';
+import qfDesign from 'qf-design';
 import { NaiveUIContainer } from '@vitepress-demo-preview/component'
 import '@vitepress-demo-preview/component/dist/style.css'
 import exTabs from './../../component/tabs/tabs.vue'
@@ -10,7 +10,7 @@ export default {
   enhanceApp: async (ctx: any) => {
     // app is the Vue 3 app instance from `createApp()`. router is VitePress'
     // custom router. `siteData`` is a `ref`` of current site-level metadata.
-    ctx.app.use(qfDesignTest);
+    ctx.app.use(qfDesign);
     ctx.app.component('demo-preview', NaiveUIContainer)
     // ctx.app.component('ex-tabs', exTabs)
 
