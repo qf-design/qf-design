8df4052622bd51332c8ee5d7e23514eef98ce670
wangkai <1205669217@qq.com>
2023-09-23 17:16:35 +0800

    feat: 1

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,14 +3,16 @@ import '@vitepress-demo-preview/component/dist/style.css'
 import DefaultTheme from 'vitepress/theme'
 import qfDesign from 'qf-design'
 import button from './../../components/button/example.vue'
-
+console.log(JSON.stringify(qfDesign), '看看这是什么')
 export default {
   ...DefaultTheme,
   enhanceApp: async (ctx: any) => {
-    // 注册一个组件
-    ctx.app.component('demo-preview', NaiveUIContainer)
     // 注册一个全局组件
     ctx.app.use(qfDesign)
+
+    // 注册一个组件
+    ctx.app.component('demo-preview', NaiveUIContainer)
+
 
   }
 }
